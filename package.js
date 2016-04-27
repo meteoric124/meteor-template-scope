175969119508e745b92bbc5cc23ed13339208652
Joey Andres <jandres@ualberta.ca>
2016-04-26 19:09:28 -0600

    Changed the versioning syntax

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "jandres:template-scope",
     summary: "Replication of the angular's $scope mechanism.",
-    version: "0.1.0-alpha15",
+    version: "0.1.0-beta.1",
     git: "https://github.com/meteoric124/meteor-template-scope.git"
 });
 
