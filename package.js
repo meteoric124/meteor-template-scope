52567d6cd3002646f2dfccb189cfecce24d45bea
Joey Andres <jandres@ualberta.ca>
2016-04-29 22:54:18 -0600

    Fixed the 0.1.0-beta.4 package to test.

--- package.js
+++ package.js
@@ -28,7 +28,7 @@ Package.onUse(function(api) {
 });
 
 Package.onTest(function(api) {
-    api.use('jandres:template-scope');
+    api.use('meteoric124:template-scope');
     api.use([
         'sanjo:jasmine@1.0.1',
         'velocity:html-reporter'
